33e15f4670fb7d480ce099abea0d5c78e24a3beb
viratupadhyay <virat.upadhyay@gmail.com>
2018-12-01 13:06:24 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,5 @@
-fname <- "./data/household_power_consumption.txt"
+setwd(dir = "~/Data Science/4. Exploratoy Data Analysis/week1")
+fname <- "./household_power_consumption.txt"
 powerdata <- read.table(fname, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 ssdata <- powerdata[powerdata$Date %in% c("1/2/2007","2/2/2007") ,]
 GAP <- as.numeric(ssdata$Global_active_power)
