aa9eeebce5681b27a4fdec102038c86e5fb14d6f
viratupadhyay <virat.upadhyay@gmail.com>
2018-12-01 13:22:21 +0100

    Update plot2.R
    Creates plot2.png

--- plot2.R
+++ plot2.R
@@ -1,9 +1,14 @@
+#Reading the data
+
 setwd(dir = "~/Data Science/4. Exploratoy Data Analysis/week1/")
 fname <- "./household_power_consumption.txt"
 powerdata <- read.table(fname, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 ssdata <- powerdata[powerdata$Date %in% c("1/2/2007","2/2/2007") ,]
 DT <- strptime(paste(ssdata$Date, ssdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 GAP <- as.numeric(ssdata$Global_active_power)
+
+#Plotting
+
 png("plot2.png", width=480, height=480)
 plot(DT, GAP, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
