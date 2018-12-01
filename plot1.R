3dc60a4343a59be2ce58c9a703aa9b1de4e75a9f
viratupadhyay <virat.upadhyay@gmail.com>
2018-12-01 13:21:44 +0100

    Update plot1.R
    Creates plot1.png

--- plot1.R
+++ plot1.R
@@ -1,8 +1,13 @@
-setwd(dir = "~/Data Science/4. Exploratoy Data Analysis/week1")
+#Reading the data
+
+setwd(dir = "~/Data Science/4. Exploratoy Data Analysis/week1/")
 fname <- "./household_power_consumption.txt"
 powerdata <- read.table(fname, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 ssdata <- powerdata[powerdata$Date %in% c("1/2/2007","2/2/2007") ,]
 GAP <- as.numeric(ssdata$Global_active_power)
+
+#Plotting
+
 png("plot1.png", width=480, height=480)
 hist(GAP, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
